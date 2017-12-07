9992ee291776c6641fb11b37fdd5003b656071ea
Michael <yiwangchunyu@163.com>
2017-12-07 15:32:56 +0800

    add test

---
+++ project4/test.h
@@ -0,0 +1,8 @@
+#include "structure.h"
+#include "counter.c"
+#include "list.c"
+#include "hash.c"
+
+
+double gettimeinterval(struct timeval start, struct timeval end);
+void *function (void *rank);
