ce0898c4ea423b5d8f0335160a0ddb3d9c5598a8
cqj8660 <cqj8660@163.com>
2017-12-09 11:42:03 +0800

    testlist is added

--- project4/testlist.c
+++ project4/testlist.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include "testlist.h"
+#define MAX 100000
 #define THREAD_COUNT 8  //线程数
 struct timeval start;
 struct timeval end;
 list_t  aList;
 //gettimeofday($start, NULL);
 
-double gettimeinterval(struct timeval start, struct timeval end)
+long int get_time_interval(struct timeval start, struct timeval end)
 {
+
 	return 1000000*(end.tv_sec-start.tv_sec) + end.tv_usec - start.tv_usec;	
+
 }
 
 int main()
@@ -18,14 +21,19 @@ int main()
 	long thread;
 	//int i;
 	pthread_t* thread_handles;
-	int thread_count = THREAD_COUNT;
+	int thread_count;
+	for(thread_count=1;thread_count<=5;thread_count++)
+	{
+	gettimeofday(&start, NULL);
 	thread_handles = malloc(thread_count*sizeof(pthread_t)); 
 	for(thread=0;thread<thread_count;thread++)
 			pthread_create(&thread_handles[thread],NULL, function, (void*)thread);
 	for(thread=0; thread<thread_count; thread++)
 		pthread_join(thread_handles[thread],NULL);
-
+	gettimeofday(&end, NULL);
+	printf("lock_type: %d, thread_count: %d, time: %ld us \n",LOCK_TYPE, thread_count, get_time_interval(start,end));	
 //	printf("%d \n", counter_get_value(&counter));
+	}
 	return 0;
 }
 
@@ -35,8 +43,8 @@ void *function (void *rank)
 	int i ;	
         //list_t t1;
 //	list_init(aList);
-//	for(i = 0 ;i<1000;i++)
-	list_insert(&aList,15);
+	for(i = 0 ;i<3;i++)
+	list_insert(&aList,i);
 	printlist(&aList);
 	return NULL;
 }
